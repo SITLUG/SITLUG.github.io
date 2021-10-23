b2f0d6470e326722f47ec871096cb028662411f2
Kevin Ha <kha@castlepointanime.com>
2021-10-23 18:17:52 -0400

    Added react

---
+++ build/static/js/main.d4adfa53.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsitlug.github.io"]=this["webpackJsonpsitlug.github.io"]||[]).push([[0],[function(i,s){}],[[0,1]]]);
+//# sourceMappingURL=main.d4adfa53.chunk.js.map
---
+++ src/components/Landing.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Landing = () => {
+    return (
+        <div className = "landing">
+            <h1>Hello World</h1>
+            <p>Welcome to the Stevens Linux Users Group website! We are a club at Stevens Institute of Technology that encourages the use and education of free and open-source software.</p>
+        </div>
+    )
+}
+
+export default Landing;
